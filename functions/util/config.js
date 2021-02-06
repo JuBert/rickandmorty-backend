736b6ad1f4cfa1370158f83e87e0a1ea2c78d81e
Julien Aubert <julien.aubert.rbs@gmail.com>
2021-02-06 11:08:26 +0100

    Route creation & Auth middleware

---
+++ functions/util/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  apiKey: 'AIzaSyATb2vUhyaCftH9N6vJJn_T_PdiuFsPtk8',
+  authDomain: 'mortyandrick-84fc9.firebaseapp.com',
+  projectId: 'mortyandrick-84fc9',
+  storageBucket: 'mortyandrick-84fc9.appspot.com',
+  messagingSenderId: '1013236884716',
+};
